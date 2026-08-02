ce01b612bceb89bfcffc84aa7d4f3d266dc20dab
Antonio Martínez (Antsoftware) <antsoftware2107@proton.me>
2026-08-02 20:40:13 +0200

    He fixeado el budgie completo por fiiin
    vale, nm-applet no queria ejecutarse asique hice que se reemplace el Exec=nm-applet por Exec=sh -c"sleep 2 && nm-applet --indicator" pa que arrancara, ahora lo voy a probar

--- void-desktop-installer.sh
+++ void-desktop-installer.sh
@@ -101,8 +101,8 @@ while true; do
             echo "Installing GNOME"
             configurar_audio_bluetooth
             sudo xbps-install -Sy xorg xdg-user-dirs NetworkManager elogind dbus nerd-fonts power-profiles-daemon gdm gnome
-            sudo rm -rf /var/service/dhcpcd/
-            sudo rm -rf /var/service/wpa_supplicant
+            sudo rm -f /var/service/dhcpcd/
+            sudo rm -f /var/service/wpa_supplicant
             sudo ln -s /etc/sv/NetworkManager /var/service/
             sudo ln -s /etc/sv/dbus/ /var/service/
             sudo ln -s /etc/sv/power-profiles-daemon/ /var/service/
@@ -117,8 +117,8 @@ while true; do
             echo "Installing KDE Plasma"
             configurar_audio_bluetooth
             sudo xbps-install -Sy xorg xdg-user-dirs NetworkManager elogind dbus nerd-fonts power-profiles-daemon sddm kde-plasma kde-baseapps ark spectacle
-            sudo rm -rf /var/service/dhcpcd/
-            sudo rm -rf /var/service/wpa_supplicant
+            sudo rm -f /var/service/dhcpcd/
+            sudo rm -f /var/service/wpa_supplicant
             sudo ln -s /etc/sv/NetworkManager /var/service/
             sudo ln -s /etc/sv/dbus/ /var/service/
             sudo ln -s /etc/sv/power-profiles-daemon/ /var/service/
@@ -133,8 +133,8 @@ while true; do
             echo "Installing Xfce"
             configurar_audio_bluetooth
             sudo xbps-install -Sy xorg xdg-user-dirs NetworkManager elogind dbus nerd-fonts power-profiles-daemon lightdm xfce4
-            sudo rm -rf /var/service/dhcpcd/
-            sudo rm -rf /var/service/wpa_supplicant
+            sudo rm -f /var/service/dhcpcd/
+            sudo rm -f /var/service/wpa_supplicant
             sudo ln -s /etc/sv/NetworkManager /var/service/
             sudo ln -s /etc/sv/dbus/ /var/service/
             sudo ln -s /etc/sv/power-profiles-daemon/ /var/service/
@@ -149,9 +149,9 @@ while true; do
             echo "Installing Budgie"
             configurar_audio_bluetooth
             sudo xbps-install -Sy xorg xdg-user-dirs NetworkManager elogind dbus adwaita-fonts nerd-fonts power-profiles-daemon \
-            lightdm budgie-desktop gnome-keyring polkit-gnome network-manager-applet nemo tilix papirus-icon-theme
-            sudo rm -rf /var/service/dhcpcd/
-            sudo rm -rf /var/service/wpa_supplicant
+            lightdm budgie-desktop gnome-keyring polkit-gnome network-manager-applet nemo tilix papirus-icon-theme udisks2
+            sudo rm -f /var/service/dhcpcd/
+            sudo rm -f /var/service/wpa_supplicant
             sudo ln -s /etc/sv/NetworkManager /var/service/
             sudo ln -s /etc/sv/dbus/ /var/service/
             sudo ln -s /etc/sv/power-profiles-daemon/ /var/service/
@@ -159,6 +159,7 @@ while true; do
             sudo ln -s /etc/sv/elogind/ /var/service/
             pkill elogind
             sudo ln -s /etc/sv/lightdm/ /var/service/
+
             cat > /etc/xdg/autostart/nm-applet.desktop << 'EOF'
 [Desktop Entry]
 Type=Application
